cbeb9ce34ead505988ed5479e347da4d1c5e7c70
João Bispo <joaobispo2077@gmail.com>
2021-10-02 15:47:09 -0300

    feat: add entity

---
+++ src/entities/Todo.ts
@@ -0,0 +1,20 @@
+import { v4 as uuid } from 'uuid';
+
+class Todo {
+  id: string;
+  user_id: string;
+  title: string;
+  done: boolean;
+  deadline: Date;
+
+  constructor(todo: Partial<Todo>) {
+    this.id = todo.id || uuid();
+
+    this.user_id = todo.user_id;
+    this.title = todo.title;
+    this.deadline = todo.deadline;
+    this.done = todo.done;
+  }
+}
+
+export { Todo };
